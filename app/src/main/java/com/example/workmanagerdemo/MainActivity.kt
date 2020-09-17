99e86c807d76bd81a315bca521e39926f892acd3
Ayusch <ayusch.jain@curefit.com>
2020-09-17 13:41:03 +0530

    Removed unused dependency

--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -2,11 +2,9 @@ package com.example.workmanagerdemo
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.*
-import com.jakewharton.rxbinding3.view.clicks
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import org.greenrobot.eventbus.EventBus
